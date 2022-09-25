214ee3a58f83d24a2d4f5d88fb375b79a8d41ffc
Indrajeet Valera <indra4you@outlook.com>
2022-09-25 21:59:51 +0530

    Improved sh file

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-git commit -am "Save uncommited changes (WIP)"
-git branch --delete --force gh-pages
-git checkout --orphan gh-pages
-git reset
-git add -f dist
-git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
+npm run build-gh-pages
+rm ./dist/*.txt
+git subtree push --prefix dist origin gh-pages
